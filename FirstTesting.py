773e72835138e61a9d72116d27985a9c868f7b0c
AnakinSy <53500346+AnakinSy@users.noreply.github.com>
2019-08-08 13:12:05 +0800

    Update FirstTesting.py
    update to verify without grid.

--- FirstTesting.py
+++ FirstTesting.py
@@ -2,10 +2,11 @@ from selenium import webdriver
 from time import sleep
 import os
 print("anakin begin...")
-driver = webdriver.Remote(
-command_executor='http://10.244.1.51:5555/wd/hub',
-desired_capabilities={'browserName': 'chrome'}
-)
+#driver = webdriver.Remote(
+#command_executor='http://10.244.1.51:5555/wd/hub',
+#desired_capabilities={'browserName': 'chrome'}
+#)
+driver = webdriver.Chrome()
 print("after remote webdriver")
 driver.get('https://www.bing.com')
 print("get bing")
