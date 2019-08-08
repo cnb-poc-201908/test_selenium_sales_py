ceddc7d1da690ed2ea6bfb3b5e1e4d5f5782a10d
AnakinSy <53500346+AnakinSy@users.noreply.github.com>
2019-08-08 11:31:26 +0800

    Add files via upload
    initial upload

---
+++ FirstTesting.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from time import sleep
+import os
+print("anakin begin...")
+driver = webdriver.Remote(
+command_executor='http://10.244.1.51:5555/wd/hub',
+desired_capabilities={'browserName': 'chrome'}
+)
+print("after remote webdriver")
+driver.get('https://www.bing.com')
+print("get bing")
+
+sleep(1)
+
+print(os.getcwd())
+
+driver.quit()
+print("end...")
