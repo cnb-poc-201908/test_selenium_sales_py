85fb51db2f6aed3e0331d02a0c841d7b98b0dfb1
AnakinSy <53500346+AnakinSy@users.noreply.github.com>
2019-08-09 09:23:22 +0800

    Update FirstTesting.py
    update

--- FirstTesting.py
+++ FirstTesting.py
@@ -6,14 +6,14 @@ print("anakin begin...")
 #command_executor='http://10.244.1.51:5555/wd/hub',
 #desired_capabilities={'browserName': 'chrome'}
 #)
-#driver = webdriver.Chrome()
-#print("after remote webdriver")
-#driver.get('https://www.bing.com')
-#print("get bing")
+driver = webdriver.Chrome()
+print("after remote webdriver")
+driver.get('https://www.bing.com')
+print("get bing")
 
 sleep(1)
 
 print(os.getcwd())
 
-#driver.quit()
+driver.quit()
 print("end...")
