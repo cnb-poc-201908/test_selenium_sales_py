a6f18a12268707fbb1e7e212b6e980f936b817f7
AnakinSy <53500346+AnakinSy@users.noreply.github.com>
2019-08-16 17:50:26 +0800

    Update FirstTesting.py
    update

--- FirstTesting.py
+++ FirstTesting.py
@@ -1,19 +1,21 @@
 from selenium import webdriver
 from time import sleep
 import os
-print("anakin begin...")
+
+print("anakin test begin...")
 driver = webdriver.Remote(
-command_executor='http://10.244.1.51:5555/wd/hub',
+command_executor='http://10.244.2.34:5555/wd/hub',
 desired_capabilities={'browserName': 'chrome'}
 )
-#driver = webdriver.Chrome()
 print("after remote webdriver")
 driver.get('https://www.bing.com')
-print("get bing")
+
+print("title is " + driver.getTitle())
 
 sleep(1)
 
 print(os.getcwd())
 
 driver.quit()
-print("end...")
+print("test end...")
+
